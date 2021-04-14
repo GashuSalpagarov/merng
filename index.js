26dea78572b32e4c336a424367e6ee484997f915
Gashu Salpagarov <gashusalpagarov@gmail.com>
2021-04-14 22:28:48 +0300

    add user registration

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
+// import { ApolloServer } from "apollo-server";
 const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers/index");
 const { MONGODB } = require("./config.js");
-
-const Post = require("./models/Post");
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
-  },
-};
 
 const server = new ApolloServer({
   typeDefs,
